f01fcebbeec11e3e67a5d3be4ed42acd87d04e61
Sourav <souravsahu70047@gmail.com>
2022-08-27 20:48:10 +0530

    Added Files

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("fun")
+// document.querySelector("#login").addEventListener("click",login)
+function login(){
+    
+    alert("Please Enter your Correct Credentials")
+    window.location.href="signup.html";
+}
+function cart(){
+    window.location.href="cart.html";
+}
+// document.querySelector(".add").addEventListener("click",show);
+function show(){
+    console.log("fun")
+}
